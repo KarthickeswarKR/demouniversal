1b0dfc4365c828b832bf362efdabf0ac9ef6a907
Karthick Eswar <karthickeswar@virtualgodown.com>
2018-10-10 16:05:22 +0530

    inital2

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
-import { AppBrowserModule } from '.././src/app/app.browser.module';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-//if (environment.production) {
+if (environment.production) {
   enableProdMode();
-//}
+}
 
-platformBrowserDynamic().bootstrapModule(AppBrowserModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule);
--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,42 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
 
-    // Work around for https://github.com/angular/angular-cli/issues/7200
-    
-    const path = require('path');
-    const webpack = require('webpack');
-    
-    module.exports = {
-      entry: {
-        server: './server.ts',
-      },
-      target: 'node',
-      resolve: { extensions: ['.ts', '.js'] },
-      externals: [/(node_modules|main\..*\.js)/,],
-      output: {
-        libraryTarget: 'commonjs2',
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].js'
-      },
-      module: {
-        rules: [
-          { test: /\.ts$/, loader: 'ts-loader' }
-        ]
-      },
-      optimization: {
-        minimize: false
-      },
-      plugins: [
-        new webpack.ContextReplacementPlugin(
-          // fixes WARNING Critical dependency: the request of a dependency is an expression
-          /(.+)?angular(\\|\/)core(.+)?/,
-          path.join(__dirname, 'src'), // location of your src
-          {} // a map of your routes
-        ),
-        new webpack.ContextReplacementPlugin(
-          // fixes WARNING Critical dependency: the request of a dependency is an expression
-          /(.+)?express(\\|\/)(.+)?/,
-          path.join(__dirname, 'src'),
-          {}
-        )
-      ]
-    }
-    
+module.exports = {
+  entry: { server: './server.ts' },
+  resolve: { extensions: ['.js', '.ts'] },
+  target: 'node',
+  mode: 'none',
+  // this makes sure we include node_modules and other 3rd party libraries
+  externals: [/node_modules/],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [{ test: /\.ts$/, loader: 'ts-loader' }]
+  },
+  plugins: [
+    // Temporary Fix for issue: https://github.com/angular/angular/issues/11580
+    // for 'WARNING Critical dependency: the request of a dependency is an expression'
+    new webpack.ContextReplacementPlugin(
+      /(.+)?angular(\\|\/)core(.+)?/,
+      path.join(__dirname, 'src'), // location of your src
+      {} // a map of your routes
+    ),
+    new webpack.ContextReplacementPlugin(
+      /(.+)?express(\\|\/)(.+)?/,
+      path.join(__dirname, 'src'),
+      {}
+    )
+  ]
+};
